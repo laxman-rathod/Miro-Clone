dc2cff8bf6aac3147c2ee71dd012e3ebba886aa3
Laxman Rathod <laxmanrathod.dev@gmail.com>
2024-07-23 22:01:16 +0530

    Add search functionality to boards and improve favorite handling - Add search functionality to boards - Implement favorites logic - Improve handling of board deletion - Update dependencies - Refactor code for better readability

--- convex/board.ts
+++ convex/board.ts
@@ -49,7 +49,17 @@ export const remove = mutation({
       throw new Error("Unauthorized!");
     }
 
-    //TODO: Later check to delete favorite relation as well
+    const userId = identity.subject;
+
+    const existingFavorites = await ctx.db
+      .query("userFavorites")
+      .withIndex("by_user_board", (q) =>
+        q.eq("userId", userId).eq("boardId", args.id)
+      )
+      .unique();
+    if (existingFavorites) {
+      await ctx.db.delete(existingFavorites._id);
+    }
 
     await ctx.db.delete(args.id);
   },
@@ -100,8 +110,8 @@ export const favorites = mutation({
 
     const existingFavorites = await ctx.db
       .query("userFavorites")
-      .withIndex("by_user_board_org", (q) =>
-        q.eq("userId", userId).eq("boardId", board._id).eq("orgId", args.orgId)
+      .withIndex("by_user_board", (q) =>
+        q.eq("userId", userId).eq("boardId", board._id)
       )
       .unique();
     if (existingFavorites) {
